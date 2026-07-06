e8f559eb59c558ff0b1c584635dca703677b2fa2
Atharva <atharvakumbhar631@gmail.com>
2026-07-06 18:18:13 +0530

    installation

---
+++ src/generated/crud-demo.ts
@@ -0,0 +1,19 @@
+import { Prisma } from "@prisma.js";
+
+async function crudDemo() {
+    const existingUser = await Prisma.user.findUnique({
+        where: {
+            email: "atharva@123.gmail.com",
+        },
+    });
+    if (!existingUser) {
+        const newUser = await Prisma.user.create({
+            data:{
+                name: "Atharva",
+                posts: {
+                    create: {title: "Hello from prisma", published: true}
+                }
+            }
+        });
+    }
+}
